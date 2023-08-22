8e384288c0c07f5e22a3ba5ea51003a65eed14a4
andvecchia <andrea.vecchiato@eng.it>
2023-08-23 00:51:59 +0200

    Implemented locate-file and colored output

--- src/commands/locate.rs
+++ src/commands/locate.rs
@@ -1,8 +1,12 @@
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Instant;
+
 use clap::Parser;
+use colored::Colorize;
 use rayon::prelude::*;
 
 #[derive(Parser, Debug)]
@@ -26,7 +30,7 @@ pub struct LocateWordArgs {
 pub struct LocateFileArgs {
     /// The directory that can contain the word in a file
     #[arg(short, long)]   
-    pub directory: String,
+    pub directory: PathBuf,
 
     /// The file to find in the directory
     #[arg(short, long)]
@@ -34,42 +38,26 @@ pub struct LocateFileArgs {
 }
 
 pub fn execute_locate_word(args: &LocateWordArgs) {
-    walk_dir(&args.directory, &args.word, &args.extensions)
+    walk_dir_word(&args.directory, &args.word, &args.extensions)
 }
 
-fn walk_dir(directory: &PathBuf, word: &str, file_types: &Option<String>) {
+fn walk_dir_word(directory: &PathBuf, word: &str, file_types: &Option<String>) {
     if let Ok(entries) = fs::read_dir(directory) {
         entries
             .par_bridge()
             .filter_map(Result::ok)
             .for_each(|entry| {
                 let path = entry.path();
-                if entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false) {
-                    walk_dir(&entry.path(), word, &file_types);
-                } else {
-                    let file_name = entry.path().display().to_string();
 
-                    if let Some(file_type) = file_types {
-                        if file_type.contains(",") {
-                            let type_list = file_type.split(",");
-                            type_list
-                                .map(|ty| ty.trim())
-                                .filter(|ty| !ty.is_empty())
-                                .for_each(|ty| {
-                                    if file_name.contains(ty) {
-                                        search_word(&path, word)
-                                    }
-                            })
-                        } else {
-                            if let Some(file_type) = file_types {
-                                if file_name.contains(file_type) {
-                                    search_word(&path, word)
-                                }
-                            }
-                        }
+                if let Ok(metadata) = path.metadata() {
+                    if metadata.is_dir() {
+                        walk_dir_word(&entry.path(), word, &file_types);
                     } else {
+                        let file_name = entry.file_name();
                         if let Some(types) = file_types {
-                            if file_name.contains(types) {
+                            if types.split(",")
+                                .filter(|ext| !ext.is_empty() && !ext.split_whitespace().next().is_none())
+                                .any(|ext| file_name.to_string_lossy().ends_with(ext.trim())) {
                                 search_word(&path, word)
                             }
                         } else {
@@ -89,14 +77,44 @@ fn search_word(dir: &PathBuf, word: &str) {
         if let Ok(item) = line {
             if item.contains(word) {
                 println!();
-                println!("Found in file: {}", dir.display());
-                println!("line: {} {}", line_number, item);
+                println!("Found in file: {}", dir.display().to_string().cyan());
+                println!("Line: {} -  {}", line_number.to_string().bright_yellow(), item.replace(word, &word.yellow().bold()
+                    .to_string()).trim().bright_black());
                 println!();
             }
         }
     }
 }
 
+fn walk_dir_file(directory: &PathBuf, file: &str, count: &AtomicUsize) {
+    if let Ok(entries) = fs::read_dir(Path::new(directory)) {
+        entries.par_bridge().filter_map(Result::ok).for_each(|entry| {
+            let path = entry.path();
+            if let Ok(metadata) = path.metadata() {
+                if metadata.is_dir() {
+                    walk_dir_file(&entry.path(), file, count);
+                } else if path.file_name().unwrap_or_default() == file {
+                    println!("{} {}", "Found in path:", path.display().to_string().bold().underline());
+                    count.fetch_add(1, Ordering::Relaxed);
+                }
+            }
+        })
+    }
+}
+
 pub fn execute_locate_file(args: &LocateFileArgs) {
-    println!("execute_locate_file {}, {}", args.directory, args.file)
+    let start = Instant::now();
+    let found_count = AtomicUsize::new(0);
+    walk_dir_file(&args.directory, &args.file, &found_count);
+    let final_count = found_count.load(Ordering::Relaxed);
+    println!();
+
+    if final_count > 0 {
+        println!("Found {} files", final_count.to_string().green());
+    } else {
+        println!("{}", "Files not found".red());
+    }
+
+    println!();
+    println!("Execution time: {:?}", start.elapsed())
 }
